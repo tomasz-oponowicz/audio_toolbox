885077dad8848f34fcf2b963896de86bfc9cd10b
Tomasz Oponowicz <tomasz.oponowicz@zaxo.biz>
2018-05-08 10:35:25 +0200

    Update url of the project

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='audio_toolbox',
       version='0.1',
       description='Handy wrapper around sox and ffmpeg',
-      url='https://bitbucket.org/tomasz_oponowicz/audio_toolbox',
+      url='https://github.com/tomasz-oponowicz/audio_toolbox',
       author='Tomasz Oponowicz',
       author_email='tomasz@zaxo.biz',
       license='MIT',
